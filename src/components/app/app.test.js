2e80a06f76e08dcc1d469f60b9baabcd88d6107a
Alex <lzlin@mail.ru>
2020-06-16 22:39:55 +1000

    пишет снепшот-тесты на созданные компоненты

---
+++ src/components/app/app.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Main from './app.jsx';
+
+it(`Render Main`, () => {
+  const tree = renderer
+    .create(<Main
+      movieDescription={
+        {
+          movieTitle: `The Grand Budapest Hotel`,
+          genre: `Comedy`,
+          release: 2014
+        }
+      }
+      titles={[
+        `Fantastic Beasts: The Crimes of Grindelwald`,
+        `Bohemian Rhapsody`, `Macbeth`,
+        `Aviator`,
+        `We need to talk about Kevin`,
+        `What We Do in the Shadows`,
+        `Revenant`,
+        `Johnny English`,
+        `Shutter Island`,
+        `Pulp Fiction`,
+        `No Country for Old Men`,
+        `Snatch`,
+        `Moonrise Kingdom`,
+        `Seven Years in Tibet`]}
+    />)
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
