1b82e627ddb7007a69f37d785e547b12dddd2be2
Alex <lzlin@mail.ru>
2020-06-19 22:32:23 +1000

    создает моки маленьких карточек

---
+++ src/mocks/films.js
@@ -0,0 +1,36 @@
+const films = [
+  {
+    title: `Fantastic Beasts: The Crimes of Grindelwald`,
+    image: `fantastic-beasts-the-crimes-of-grindelwald`
+  },
+  {
+    title: `Bohemian Rhapsody`,
+    image: `bohemian-rhapsody`
+  },
+  {
+    title: `Macbeth`,
+    image: `macbeth`
+  },
+  {
+    title: `Aviator`,
+    image: `aviator`
+  },
+  {
+    title: `Revenant`,
+    image: `revenant`
+  },
+  {
+    title: `Snatch`,
+    image: `snatch`
+  },
+  {
+    title: `Orlando`,
+    image: `orlando`
+  },
+  {
+    title: `Mindhunter`,
+    image: `mindhunter`
+  },
+];
+
+
