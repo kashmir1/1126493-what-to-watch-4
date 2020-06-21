50a4ceaa295dc7b1990a2748272bddf87fcd4279
Alex <lzlin@mail.ru>
2020-06-21 16:41:38 +1000

    создает тест для компонента списка фильмов

---
+++ src/components/movie-list/movie-list.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import MoviesList from './movie-list.jsx';
+
+const moviesList = [
+  {
+    id: 0,
+    title: `Fantastic Beasts: The Crimes of Grindelwald`,
+    image: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`
+  },
+  {
+    id: 1,
+    title: `Bohemian Rhapsody`,
+    image: `img/bohemian-rhapsody.jpg`
+  },
+  {
+    id: 2,
+    title: `Macbeth`,
+    image: `img/macbeth.jpg`
+  },
+];
+
+it(`MovieList is rendered correctly`, () => {
+  const onCardMouseHover = jest.fn();
+  const onTitleClick = jest.fn();
+  const tree = renderer.create(
+      <MoviesList
+        moviesList={moviesList}
+        onCardMouseHover={onCardMouseHover}
+        onTitleClick={onTitleClick}
+      />
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
+});
