dabaab41528e1529592737668b3ec6b635cc059f
Alex <lzlin@mail.ru>
2020-06-22 23:07:05 +1000

    удаляет неиспользуемый массив заголовков

--- src/index.js
+++ src/index.js
@@ -10,8 +10,6 @@ const MovieDescription = {
   release: 2014
 };
 
-const MoviesList = [`Fantastic Beasts: The Crimes of Grindelwald`, `Bohemian Rhapsody`, `Macbeth`, `Aviator`, `We need to talk about Kevin`, `What We Do in the Shadows`, `Revenant`, `Johnny English`, `Shutter Island`, `Pulp Fiction`, `No Country for Old Men`, `Snatch`, `Moonrise Kingdom`, `Seven Years in Tibet`];
-
 ReactDOM.render(
     <App
       movieDescription={MovieDescription}
