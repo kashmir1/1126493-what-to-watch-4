c7e748859011be1257c3951b4725b48eebd1f424
Alex <lzlin@mail.ru>
2020-07-03 22:31:58 +1000

    меняет MovieCard на класс-компонент

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,26 +1,33 @@
-import React from "react";
+import React, {PureComponent} from "react";
 import {moviesListType} from '../../types/index';
 
-const MovieCard = (moviesListProps) => {
-  const {movie, onCardMouseHover, onTitleClick} = moviesListProps;
-  const {title, src} = movie;
-  return (
-    <article className="small-movie-card catalog__movies-card" onMouseEnter={onCardMouseHover}>
-      <div className="small-movie-card__image"
-        onClick={() => onTitleClick(movie)}
-        onMouseEnter={() => onCardMouseHover(movie)}
-      >
-        <img src={src} alt={title} width="280" height="175" />
-      </div>
-      <h3 className="small-movie-card__title">
-        <a onClick={(evt) => {
-          evt.preventDefault();
-          onTitleClick(movie);
-        }} className="small-movie-card__link" href="movie-page.html">{title}</a>
-      </h3>
-    </article>
-  );
-};
+class MovieCard extends PureComponent {
+  constructor(moviesListProps) {
+    super(moviesListProps);
+    this.moviesListProps = moviesListProps;
+  }
+
+  render() {
+    const {movie, onCardMouseHover, onTitleClick} = this.moviesListProps;
+    const {title, src} = movie;
+    return (
+      <article className="small-movie-card catalog__movies-card" onMouseEnter={onCardMouseHover}>
+        <div className="small-movie-card__image"
+          onClick={() => onTitleClick(movie)}
+          onMouseEnter={() => onCardMouseHover(movie)}
+        >
+          <img src={src} alt={title} width="280" height="175" />
+        </div>
+        <h3 className="small-movie-card__title">
+          <a onClick={(evt) => {
+            evt.preventDefault();
+            onTitleClick(movie);
+          }} className="small-movie-card__link" href="movie-page.html">{title}</a>
+        </h3>
+      </article>
+    );
+  }
+}
 
 MovieCard.propTypes = {
   moviesListProps: moviesListType,
