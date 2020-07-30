459e426c941d1e69a9ddc27b9caeb58deb45a25c
Alex <lzlin@mail.ru>
2020-07-30 21:52:20 +1000

    создает и подключает компонент хедера

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -8,6 +8,8 @@ import {ALL_GENRES} from "../../const";
 import MovieNavGenre from '../movie-nav-genre/movie-nav-genre.jsx';
 import MoviesList from "../movie-list/movies-list.jsx";
 import ShowMore from "../show-more/show-more.jsx";
+import Header from '../header/header.jsx';
+import Footer from '../footer/footer.jsx';
 
 const COUNT_OF_SHOW_FILMS = 8;
 
@@ -43,21 +45,7 @@ class Main extends PureComponent {
 
         <h1 className="visually-hidden">WTW</h1>
 
-        <header className="page-header movie-card__head">
-          <div className="logo">
-            <a className="logo__link">
-              <span className="logo__letter logo__letter--1">W</span>
-              <span className="logo__letter logo__letter--2">T</span>
-              <span className="logo__letter logo__letter--3">W</span>
-            </a>
-          </div>
-
-          <div className="user-block">
-            <div className="user-block__avatar">
-              <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
-            </div>
-          </div>
-        </header>
+        <Header />
 
         <div className="movie-card__wrap">
           <div className="movie-card__info">
@@ -117,19 +105,7 @@ class Main extends PureComponent {
           }}/>}
         </section>
 
-        <footer className="page-footer">
-          <div className="logo">
-            <a className="logo__link logo__link--light">
-              <span className="logo__letter logo__letter--1">W</span>
-              <span className="logo__letter logo__letter--2">T</span>
-              <span className="logo__letter logo__letter--3">W</span>
-            </a>
-          </div>
-
-          <div className="copyright">
-            <p>© 2019 What to watch Ltd.</p>
-          </div>
-        </footer>
+        <Footer />
       </div>
     </React.Fragment>);
   }
