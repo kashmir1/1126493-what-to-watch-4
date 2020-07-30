1e2622e841b10dec00f87534196bcdbe64a819f6
Alex <lzlin@mail.ru>
2020-07-30 10:30:25 +1000

    описывает логику кнопки showmore

--- src/components/show-more/show-more.jsx
+++ src/components/show-more/show-more.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const ShowMore = () => {
+
+const ShowMore = (props) => {
+  const {onShowMoreClick} = props;
+
   return (
     <div className="catalog__more">
-      <button className="catalog__button" type="button">Show more</button>
+      <button className="catalog__button" type="button"
+        onClick={(evt) => {
+          evt.preventDefault();
+          onShowMoreClick();
+        }}
+      >Show more</button>
     </div>
   );
+};
+
+ShowMore.propTypes = {
+  onShowMoreClick: PropTypes.func.isRequired,
 };
 
 export default ShowMore;
