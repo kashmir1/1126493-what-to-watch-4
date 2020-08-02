bfc2d0a05cf0c51048abc2d1150188be85297df8
Alex <lzlin@mail.ru>
2020-08-03 08:49:23 +1000

    линтер

--- src/components/movie-nav-reviews/movie-nav-reviews.test.js
+++ src/components/movie-nav-reviews/movie-nav-reviews.test.js
@@ -3,7 +3,6 @@ import renderer from 'react-test-renderer';
 import MovieNavReviews from './movie-nav-reviews.jsx';
 import {comments} from '../data-for-test.js';
 
-
 describe(`MovieNavReviews`, () => {
   it(`Render MovieNavReviews`, () => {
     const tree = renderer.create(
