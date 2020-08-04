bcac67b3a8a90b31dd0b113f3aeaf4a3b3661103
Alex <lzlin@mail.ru>
2020-08-04 14:12:58 +1000

    обновляет тесты

--- src/components/video-player/video-player.test.js
+++ src/components/video-player/video-player.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import VideoPlayer from './video-player.jsx';
-import films from "../../mocks/testing.js";
+import {films} from '../data-for-test.js';
 
 const film = films[0];
 
@@ -17,6 +17,8 @@ describe(`VideoPlayer`, () => {
             return {};
           }
         }).toJSON();
+
     expect(tree).toMatchSnapshot();
   });
 });
+
