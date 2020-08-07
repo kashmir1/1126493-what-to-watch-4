6194775107a627da626fa591428a9e85afa2c3aa
Alex <lzlin@mail.ru>
2020-08-07 19:38:44 +1000

    удаляет мусор

--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,4 @@
-import {ALL_GENRES} from './const.js';
 import moment from "moment";
-
-export const getAvailableGenres = (films) => {
-  const availableGenre = new Set(films.map((film) => film.genre));
-  return [ALL_GENRES, ...availableGenre];
-};
 
 export const formatRuntime = (runtime) => {
   const hours = moment.duration(runtime, `minutes`).hours();
